4ee5cd4ef0da613140d40822507f1de0d3cbedfc
ASUS <m>
2021-08-09 07:02:57 +0200

    Replaced uiScope with viewModelScope because viewModelScope uses the Main Thread by default but also handles cancelling the Job if the ViewModel is destroyed.

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -34,9 +34,6 @@ class SleepTrackerViewModel(
         val database: SleepDatabaseDao,
         application: Application) : AndroidViewModel(application) {
 
-        private val viewModelJob = Job()
-        private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
-
         private var tonight = MutableLiveData<SleepNight?>()
 
         private val nights = database.getAllNights()
@@ -53,7 +50,7 @@ class SleepTrackerViewModel(
         }
 
         private fun initializeTonight() {
-                uiScope.launch {
+                viewModelScope.launch {
                         tonight.value = getTonightFromDatabase()
                 }
         }
@@ -75,25 +72,9 @@ class SleepTrackerViewModel(
                 }
         }
 
-        fun onStartTracking() {
-                uiScope.launch {
-                        val newNight = SleepNight()
-                        insert(newNight)
-                        tonight.value = getTonightFromDatabase()
-                }
-        }
-
-        private suspend fun insert(night: SleepNight) {
-                return withContext(Dispatchers.IO) {
-                        database.insert(night)
-                }
-        }
-
-        fun onStopTracking() {
-                uiScope.launch {
-                        val oldNight = tonight.value ?: return@launch
-                        oldNight.endTimeMilli = System.currentTimeMillis()
-                        update(oldNight)
+        private suspend fun clear() {
+                withContext(Dispatchers.IO) {
+                        database.clear()
                 }
         }
 
@@ -103,29 +84,56 @@ class SleepTrackerViewModel(
                 }
         }
 
-        fun onClear() {
-                uiScope.launch {
-                        clear()
-                        tonight.value = null
-                }
-        }
-
-        private suspend fun clear() {
-                withContext(Dispatchers.IO) {
-                        database.clear()
+        private suspend fun insert(night: SleepNight) {
+                return withContext(Dispatchers.IO) {
+                        database.insert(night)
                 }
         }
 
         /**
-         * This method will be called when this ViewModel is no longer used and will be destroyed.
-         *
-         *
-         * It is useful when ViewModel observes some data and you need to clear this subscription to
-         * prevent a leak of this ViewModel.
+         * Executes when the START button is clicked.
          */
-        override fun onCleared() {
-                super.onCleared()
-                viewModelJob.cancel()
+        fun onStartTracking() {
+                viewModelScope.launch {
+                        // Create a new night, which captures the current time,
+                        // and insert it into the database.
+                        val newNight = SleepNight()
+
+                        insert(newNight)
+
+                        tonight.value = getTonightFromDatabase()
+                }
+        }
+
+        /**
+         * Executes when the STOP button is clicked.
+         */
+        fun onStopTracking() {
+                viewModelScope.launch {
+                        // In Kotlin, the return@label syntax is used for specifying which function among
+                        // several nested ones this statement returns from.
+                        // In this case, we are specifying to return from launch(),
+                        // not the lambda.
+                        val oldNight = tonight.value ?: return@launch
+
+                        // Update the night in the database to add the end time.
+                        oldNight.endTimeMilli = System.currentTimeMillis()
+
+                        update(oldNight)
+                }
+        }
+
+        /**
+         * Executes when the CLEAR button is clicked.
+         */
+        fun onClear() {
+                viewModelScope.launch {
+                        // Clear the database table.
+                        clear()
+
+                        // And clear tonight since it's no longer in the database
+                        tonight.value = null
+                }
         }
 }
 
